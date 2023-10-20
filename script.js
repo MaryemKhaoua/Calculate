a50f08d91685dc3c5f6a2cc1318d9eabca74dadf
Maryem Khaoua <maryem.khaoua@gmail.com>
2023-10-20 16:36:30 +0100

    add a modification in js file

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const inputt =document.getElementById('display');
 var operation;
+
+function afficher(n){
+   inputt.innerHTML+=n;
+   if(n=='+' || n=='-' ||n=='*' || n=='/' ){
+    operation=n;
+   }
+  
+}
+
+function delet(){
+    inputt.innerHTML='';
+}
 function calcule()
 {
     switch(operation)
