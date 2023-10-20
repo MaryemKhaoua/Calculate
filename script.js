1e98453656af264b70199eb6aa437e638183f5bc
Maryem Khaoua <maryem.khaoua@gmail.com>
2023-10-20 16:30:34 +0100

    add code of - opperation

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-consol.log
+const inputt =document.getElementById('display');
+var operation;
+function calcule()
+{
+    switch(operation)
+    {
+        case '-':
+            var table = inputt.innerText.split('-');
+            var diff =table[0];
+            for(let i =1 ; i <table.length;i++){
+                diff -= Number(table[i]) ;
+            }
+            inputt.innerHTML= diff;
+        break;
+    }
+    }
