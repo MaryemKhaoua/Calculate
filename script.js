85dc5578a372cf77c2c035905fc39a86ebd7421c
oussama <freegiftcardishere@gmail.com>
2023-10-20 16:42:50 +0200

    adding the function of adding

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-consol.log
+const inputt =document.getElementById('display');
+var operation;
+
+function afficher(n){
+   inputt.innerHTML+=n;
+   if(n=='+' || n=='-' || n=='' || n=='/' ){
+    operation=n;
+   }
+
+}
+
+function delet(){
+    inputt.innerHTML='';
+}
+
+function calcule(){
+    switch(operation){
+        case '+':
+            var table = inputt.innerText.split('+');
+            var sum =0;
+            for(let i =0 ; i <table.length;i++){
+                sum += Number(table[i]);
+            }
+            inputt.innerHTML= sum;
+            break;
+        }
+    }
